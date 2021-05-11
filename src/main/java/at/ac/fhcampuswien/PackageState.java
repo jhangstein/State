429e6a150872cd09d526f3474462f1229bad020f
jhangstein <joseph.hangstein@stud.fhcampus-wien.ac.at>
2021-05-11 11:58:20 +0200

    State

---
+++ src/main/java/at/ac/fhcampuswien/PackageState.java
@@ -0,0 +1,7 @@
+package at.ac.fhcampuswien;
+
+public interface PackageState {
+    void next(Package pkg);
+    void prev(Package pkg);
+    void printStatus();
+}
---
+++ src/main/java/at/ac/fhcampuswien/ReceivedState.java
@@ -0,0 +1,18 @@
+package at.ac.fhcampuswien;
+
+public class ReceivedState implements PackageState{
+    @Override
+    public void next(Package pkg) {
+        System.out.println("This package is already received by a client.");
+    }
+
+    @Override
+    public void prev(Package pkg) {
+        pkg.setState(new DeliveredState());
+    }
+
+    @Override
+    public void printStatus(){
+        System.out.println("This package has been received by a client.");
+    }
+}
